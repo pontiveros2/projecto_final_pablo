a0d9ae92bc9d5a00c29332c18eddf05eca76674d
Pablo Ontiveros Lozano <169077179+pontiveros2@users.noreply.github.com>
2024-08-24 20:08:38 -0600

    Add files via upload

--- aplicacion_prueba/routes.py
+++ aplicacion_prueba/routes.py
@@ -8,47 +8,45 @@ class RouteManager:
     def __init__(self):
         self.current_window = None # None especifica que estara vacia
 
-
-
     def dirigir_a_login(self):
-
         token_actual = load_token()
-
         if token_actual != '' and token_is_valid(token_actual):
             self.nombre_usuario = obtenet_usuario_token(token_actual)
-            self.mandar_a_principal()
-            self.cerrar_sesion()
+            self.mandar_a_principal(token=token_actual)
             print('Exite un token')
         else:
+            print('No hay token o ya expiro')
             self.mandar_a_login()
 
-            print('No hay token o ya expiro')
-
-
-
-    # Mandar al usuario a la vista login
-    def mandar_a_login(self, ventana_anterior):
+    def mandar_a_login(self, ventana_anterior=None): # Mandar al usuario a la vista login
         from mainwindow import MainWindow  # Importación diferida
-        self.current_window = MainWindow() #Asignamos la ventana mainwindow
-        self.current_window.show() # mostramos la ventana mainwindow
-        ventana_anterior.close() # Se cierra la ventana actual
+        print(ventana_anterior)
+        if ventana_anterior:
+            self.current_window = MainWindow() # La ventana actual se conviernte en MainWindow
+            self.current_window.show() # Mostramos la ventana actual
+            ventana_anterior.close() # Se cierra la anterior
+        else:
+            self.current_window = MainWindow()
+            self.current_window.show() # Mostramos la ventana actual
 
-    # Mandar al usuario a la vista registro
-    def mandar_a_registro(self, ventana_anterior):
+    def mandar_a_registro(self, ventana_anterior): # Mandar al usuario a la vista registro
         from register import RegisterWindow  # Importación diferida
         self.current_window = RegisterWindow() #Asignamos la ventana de register
         self.current_window.show() # mostramos la ventana register
         ventana_anterior.close() # Se cierra la ventana actual
 
-    # Mandar al usuario a la vista home
-    def mandar_a_principal(self, ventana_anterior, token):
+    def mandar_a_principal(self, ventana_anterior = None, token=None):  # Mandar al usuario a la vista home
         from home import HomeWindow  # Importación diferida
 
-        save_token(token)
-#--------------------------------------------------------------
-        info_user = decode_token(token)
-        nombre_usuario = info_user['sub']
-
-        self.current_window = HomeWindow(nombre_usuario=nombre_usuario)
-        self.current_window.show()
-        ventana_anterior.close() # Se cierra la ventana actual
+        if ventana_anterior:
+            save_token(token)
+            info_user = decode_token(token)
+            nombre_usuario = info_user['sub'] # Subject (a quien le pertenece el token)
+            self.current_window = HomeWindow(nombre_usuario=nombre_usuario)
+            self.current_window.show()
+            ventana_anterior.close() # Se cierra la ventana actual
+        else:
+            info_user = decode_token(token)
+            nombre_usuario = info_user['sub'] # Subject (a quien le pertenece el token)
+            self.current_window = HomeWindow(nombre_usuario=nombre_usuario)
+            self.current_window.show() # Mostramos la ventana actual
